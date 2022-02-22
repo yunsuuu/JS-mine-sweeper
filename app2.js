1e39120e6366f03f7619ac5734b606e24c80ae61
dbstnqls6953@naver.com <dbstnqls6953@naver.com>
2022-02-22 23:53:48 +0900

    .

---
+++ app2.js
@@ -0,0 +1,6 @@
+const $form = document.querySelector("#form");
+const $body = document.querySelector("table tbody");
+const $result = document.querySelector("#result");
+const row = 10;
+const cell = 10;
+const mine = 10;
